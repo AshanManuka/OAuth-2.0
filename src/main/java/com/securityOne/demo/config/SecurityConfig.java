55edad1e7bb280caae93747b0b7e541ae0ebb83d
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-09-20 15:29:48 +0530

    controll access

---
+++ src/main/java/com/securityOne/demo/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.securityOne.demo.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+    }
+
+//            .authorizeRequests()
+//            .antMatchers("/").permitAll()
+//                .anyRequest().authenticated()
+//            .and()
+//                .oauth2Login();
+}
