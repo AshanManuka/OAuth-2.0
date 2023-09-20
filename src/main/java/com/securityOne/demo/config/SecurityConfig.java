e80fcd7973f3c3973e19ee8e638b9e8cae9eeab3
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-09-20 16:05:56 +0530

    close session  by logout

--- src/main/java/com/securityOne/demo/config/SecurityConfig.java
+++ src/main/java/com/securityOne/demo/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,6 +19,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
                 )
                 .oauth2Login();
     }
